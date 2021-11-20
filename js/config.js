216986c147473689ca7ade9dec1131acb0662163
wt423 <77811675+wt423@users.noreply.github.com>
2021-11-19 19:45:15 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_qZYTDw6fe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7f7mlmc352sc7hpg37fvrgt5gt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
